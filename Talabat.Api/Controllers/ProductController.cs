1f21dccbf749dce6462037945e76737f3ed940ff
OmarWageh <omarwagih815@gmail.com>
2026-07-20 13:45:55 +0300

    change some pronlem in productController

--- Talabat.Api/Controllers/ProductController.cs
+++ Talabat.Api/Controllers/ProductController.cs
@@ -33,28 +33,29 @@ namespace Talabat.Api.Controllers
         public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts([FromQuery] string? search,
          [FromQuery] string? sort,
           [FromQuery] string? order,
-         [FromQuery] int ? ProductBrandId,
+         [FromQuery] int? ProductBrandId,
          [FromQuery] int? CategoryId,
-         [FromQuery] int pageIndex=1,
+         [FromQuery] int pageIndex = 1,
          [FromQuery] int pageSize = 10)
         {
             if (pageIndex <= 0 || pageSize <= 0)
             {
                 return BadRequest(new ApiResponseToNotFound_Badrequest_Unauthorized(StatusCodes.Status400BadRequest, "pageIndex and pageSize must be greater than zero."));
             }
-            var products = await _productRepo.GetProducts(search,sort
+            var products = await _productRepo.GetProducts(search, sort
                , order, ProductBrandId, CategoryId, pageIndex, pageSize);
             var data = _mapper.Map<IEnumerable<ProductToReturnDto>>(products);
-             var response = new PagedResposeDto<ProductToReturnDto>()
+            var response = new PagedResposeDto<ProductToReturnDto>()
             {
                 PageIndex = pageIndex,
                 PageSize = pageSize,
-                TotalCount= await _productRepo.GetProductsCountAsync(search),
-                 Data = data,
-              
+                TotalCount = await _productRepo.GetProductsCountAsync(search),
+                Data = data,
+
             };
             return Ok(response);
         }
+      
         //this endpoint to get product by id 
         [ProducesResponseType(typeof(ProductToReturnDto), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponseToNotFound_Badrequest_Unauthorized), StatusCodes.Status404NotFound)]
@@ -63,7 +64,7 @@ namespace Talabat.Api.Controllers
         {
             if (id <= 0)
                 return NotFound(new ApiResponseToNotFound_Badrequest_Unauthorized(StatusCodes.Status400BadRequest, "Invalid product id."));
-            var product = await _productRepo.GetAsync(id);
+            var product = await _unitOfWork.Repository<Product>().GetAsync(id);
             if (product == null)
             {
                 _logger.LogInformation("Product with id {ProductId} not found.", id);
